ed62d2ce154e98815399c3e80dd6bfc4b4324ef5
AndrewAnrzumaniyan <imsobaka@mail.ru>
2024-02-25 16:32:17 +0300

    add yandex_metric

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,34 @@ export default function Document() {
               `  
             }}
           />
+
+          {/* Яндекс Метрика */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+              ym(96568720, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });`,
+            }}
+          />
+          <noscript>
+            <div>
+              <img 
+                src="https://mc.yandex.ru/watch/96568720" 
+                style={{
+                  position: 'absolute', 
+                  left: '-9999px'
+                }}
+                alt="" 
+              />
+            </div>
+          </noscript>
       </Head>
       <body>
         <Main />
